1909fa2f712eedc7419bb5a71c7d75f71201150b
Tom Soulanille <soul@prama.com>
2021-04-20 17:26:30 -0400

    start.sh: tensorboard port, TFDS_DATA_DIR

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #!/usr//bin/bash
 # FIXME: put directory info in name
-docker run --gpus all -d -it -p 8848:8888 -v $(pwd):/home/jovyan/work -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --user root --restart on-failure:16 --name gpu-jupyter_1 gpu-jupyter
+### docker run --gpus all -d -it -p 8848:8888 -v $(pwd):/home/jovyan/work -v /data-pool/nn:/data -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --user root --restart on-failure:16 --name gpu-jupyter gpu-jupyter
+docker run --gpus all -d -it --rm \
+       -p 6006:6006 \
+       -p 8848:8888 \
+       -v $(pwd):/home/jovyan/work \
+       -v /data-pool/nn:/data \
+       -e TFDS_DATA_DIR=/data/tfds \
+       -e GRANT_SUDO=yes \
+       -e JUPYTER_ENABLE_LAB=yes \
+       --user root \
+       --name gpu-jupyter \
+       gpu-jupyter
