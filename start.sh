98333b875f6c6cda6dee86e6eab02c5ddc622543
Tom Soulanille <soul@koi.localdomain>
2021-04-20 22:06:51 +0000

    start.sh: use env to find bash

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/usr//bin/bash
+#!/usr/bin/env bash
 # FIXME: put directory info in name
 ### docker run --gpus all -d -it -p 8848:8888 -v $(pwd):/home/jovyan/work -v /data-pool/nn:/data -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes --user root --restart on-failure:16 --name gpu-jupyter gpu-jupyter
 docker run --gpus all -d -it --rm \
